d2060283a94f779624db0f6c6f92cf284828b15a
Hongwei Xi <gmhwxi@gmail.com>
2016-07-15 23:39:38 -0400

    Updating: very very minorly

--- SCRIPT/C9-ATS2-install-cs520-z3.sh
+++ SCRIPT/C9-ATS2-install-cs520-z3.sh
@@ -100,6 +100,12 @@ sudo apt-get install -y erlang
 (cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2py3 && mv -f atscc2py3 ${PATSHOME}/bin)
 (cd ATS2-contrib/contrib/libatscc/libatscc2py3 && time make all && time make all_in_one)
 #
+# For building atscc2cli
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2cli && time make build)
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2cli && mv -f atscc2cli ${PATSHOME}/bin)
+(cd ATS2-contrib/contrib/libatscc/libatscc2cli && time make all && time make all_in_one)
+#
 # For building atscc2erl
 #
 (cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2erl && time make build)
